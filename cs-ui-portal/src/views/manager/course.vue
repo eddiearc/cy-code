61812276d247fd5c8e8cf51ee41d0c3fe08ea40b
anxuvim <1010597410@qq.com>
2021-01-12 11:06:06 +0800

    bf

--- cs-ui-portal/src/views/manager/course.vue
+++ cs-ui-portal/src/views/manager/course.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.key" placeholder="关键字" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+      <el-input v-model="listQuery.name" placeholder="请输入课程名称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
       <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
         搜索
       </el-button>
